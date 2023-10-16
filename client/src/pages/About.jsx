4fbdcf58a9d766ce945746a25a42b753090ce824
Preetam Gupta <106731801+PreetamGup@users.noreply.github.com>
2023-10-16 13:02:23 +0530

    image link

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -55,7 +55,7 @@ const About = () => {
             </div>
 
             <div className='aboutImage mt-3'>
-                <img src="https://media.licdn.com/dms/image/C4D22AQG7rFQLeiks4A/feedshare-shrink_1280/0/1675856659235?e=1695859200&v=beta&t=OPGdu-tDecJQpRqOU-FawcH0Il8RaU9wGy0PhFHQGsw" alt="About" />
+                <img src="https://media.licdn.com/dms/image/C4D22AQG7rFQLeiks4A/feedshare-shrink_800/0/1675856659235?e=1700092800&v=beta&t=DRcbgbSI2K5hjgEErzgD_7NPaePphY8NyyIguLJh_M0" alt="About" />
             </div>
             
         </div>
